2454cf43d1b6db84075018c4830b8a75871bee2a
nooobcoder <suryashi2013@gmail.com>
2022-03-05 21:06:02 +0530

    Use Classes with beans and properties

--- Track 2_JAVA and SQL/SpringFramework/src/main/java/org/nooobcoder/App.java
+++ Track 2_JAVA and SQL/SpringFramework/src/main/java/org/nooobcoder/App.java
@@ -6,8 +6,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("ApplicationContext.xml");
-        Vehicle obj = (Vehicle) applicationContext.getBean("bike");
-        obj.drive();
+        /*Vehicle obj = (Vehicle) applicationContext.getBean("bike");
+        obj.drive();*/
 
+        Tyre tyre = (Tyre) applicationContext.getBean("tyre");
+        System.out.println(tyre);
     }
 }
--- Track 2_JAVA and SQL/SpringFramework/src/main/java/org/nooobcoder/Tyre.java
+++ Track 2_JAVA and SQL/SpringFramework/src/main/java/org/nooobcoder/Tyre.java
@@ -1,2 +1,20 @@
-package org.nooobcoder;public class Tyre {
+package org.nooobcoder;
+
+public class Tyre {
+    private String brand;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    @Override
+    public String toString() {
+        return "Tyre{" +
+                "brand='" + brand + '\'' +
+                '}';
+    }
 }
