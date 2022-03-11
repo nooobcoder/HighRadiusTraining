fbf579e98dcef140b479bcc931e214832c500de3
nooobcoder <suryashi2013@gmail.com>
2022-03-11 12:30:40 +0530

    Enable JIT Tailwind

--- Track 4_ReactJS_Web Development/Project/Frontend/hrc-dashboard/tailwind.config.js
+++ Track 4_ReactJS_Web Development/Project/Frontend/hrc-dashboard/tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  mode: 'jit',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
