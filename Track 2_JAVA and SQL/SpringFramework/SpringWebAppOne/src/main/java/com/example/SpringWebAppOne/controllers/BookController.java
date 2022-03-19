64b14278e1134d5213776d1a30a7aec447da2948
Ankur Paul <suryashi2013@gmail.com>
2022-03-19 20:34:46 +0530

    Add SpringPetClinic Template

---
+++ Track 2_JAVA and SQL/SpringFramework/SpringWebAppOne/src/main/java/com/example/SpringWebAppOne/controllers/BookController.java
@@ -0,0 +1,22 @@
+package com.example.SpringWebAppOne.controllers;
+
+import com.example.SpringWebAppOne.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "books/list";
+    }
+}
