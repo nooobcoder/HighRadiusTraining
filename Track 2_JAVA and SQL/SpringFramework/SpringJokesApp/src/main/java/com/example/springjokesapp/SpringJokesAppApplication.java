a02e6aac84094d041e27c63aa3b83f88aaa15355
Ankur Paul <suryashi2013@gmail.com>
2022-03-21 20:20:55 +0530

    Implement service: JokeService for the app. Closes #13

--- Track 2_JAVA and SQL/SpringFramework/SpringJokesApp/src/main/java/com/example/springjokesapp/SpringJokesAppApplication.java
+++ Track 2_JAVA and SQL/SpringFramework/SpringJokesApp/src/main/java/com/example/springjokesapp/SpringJokesAppApplication.java
@@ -2,12 +2,13 @@ package com.example.springjokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringJokesAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringJokesAppApplication.class, args);
+        ApplicationContext context = SpringApplication.run(SpringJokesAppApplication.class, args);
     }
 
 }
---
+++ Track 2_JAVA and SQL/SpringFramework/SpringJokesApp/src/main/java/com/example/springjokesapp/services/JokeService.java
@@ -0,0 +1,5 @@
+package com.example.springjokesapp.services;
+
+public interface JokeService {
+    String getJoke();
+}
