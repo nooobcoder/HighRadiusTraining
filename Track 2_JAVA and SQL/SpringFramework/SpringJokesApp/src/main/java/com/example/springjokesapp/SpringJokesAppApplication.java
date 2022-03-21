54d5773bd2eb31c68e2e29b7f9abf8e17dc28254
Ankur Paul <suryashi2013@gmail.com>
2022-03-21 20:11:02 +0530

    BootStrap Project and link maven dependencies. Closes #12

---
+++ Track 2_JAVA and SQL/SpringFramework/SpringJokesApp/src/main/java/com/example/springjokesapp/SpringJokesAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJokesAppApplication.class, args);
+    }
+
+}
---
+++ Track 2_JAVA and SQL/SpringFramework/SpringJokesApp/src/test/java/com/example/springjokesapp/SpringJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
