f6b99482da80c982f5b9c2879bee421f74415e4a
Ankur Paul <suryashi2013@gmail.com>
2022-03-25 18:10:01 +0530

    Create API Endpoint /getBusinesses
    This feature adds an endpoint to fetch the businesses from the business table.

--- Track 4_ReactJS_Web Development/Project/Backend/API/demo/src/main/java/pojo/BusinessPOJO.java
+++ Track 4_ReactJS_Web Development/Project/Backend/API/demo/src/main/java/pojo/BusinessPOJO.java
@@ -1,2 +1,37 @@
-package pojo;public class BusinessPOJO {
+package pojo;
+
+public class BusinessPOJO {
+    String business_code, business_name;
+
+    public BusinessPOJO(String business_code, String business_name) {
+        this.business_code = business_code;
+        this.business_name = business_name;
+    }
+
+    public BusinessPOJO() {
+    }
+
+    public String getBusiness_code() {
+        return business_code;
+    }
+
+    public void setBusiness_code(String business_code) {
+        this.business_code = business_code;
+    }
+
+    public String getBusiness_name() {
+        return business_name;
+    }
+
+    public void setBusiness_name(String business_name) {
+        this.business_name = business_name;
+    }
+
+    @Override
+    public String toString() {
+        return "BusinessPOJO{" +
+                "business_code='" + business_code + '\'' +
+                ", business_name='" + business_name + '\'' +
+                '}';
+    }
 }
--- Track 4_ReactJS_Web Development/Project/Backend/API/demo/src/main/java/pojo/CustomerPOJO.java
+++ Track 4_ReactJS_Web Development/Project/Backend/API/demo/src/main/java/pojo/CustomerPOJO.java
@@ -1,2 +1,38 @@
-package pojo;public class CustomerPOJO {
+package pojo;
+
+public class CustomerPOJO {
+    int cust_number;
+    String name_customer;
+
+    public CustomerPOJO() {
+    }
+
+    public CustomerPOJO(int cust_number, String name_customer) {
+        this.cust_number = cust_number;
+        this.name_customer = name_customer;
+    }
+
+    public int getCust_number() {
+        return cust_number;
+    }
+
+    public void setCust_number(int cust_number) {
+        this.cust_number = cust_number;
+    }
+
+    public String getName_customer() {
+        return name_customer;
+    }
+
+    public void setName_customer(String name_customer) {
+        this.name_customer = name_customer;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerPOJO{" +
+                "cust_number=" + cust_number +
+                ", name_customer='" + name_customer + '\'' +
+                '}';
+    }
 }
