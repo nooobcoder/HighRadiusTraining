86d223a0a1d2837f4fd538ae0fe3bb7c718999d4
nooobcoder <suryashi2013@gmail.com>
2022-04-07 17:40:52 +0530

    Sticky footer bottom and table max-w 96 rem

--- Track 4_ReactJS_Web Development/Project/Frontend/hrc-dashboard/src/components/Footer.jsx
+++ Track 4_ReactJS_Web Development/Project/Frontend/hrc-dashboard/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="bottom-0 p-3 mt-10 bg-gray-800 rounded-t-lg shadow sm:text-xs md:flex md:justify-between md:items-center md:p-5">
+    <footer className="fixed bottom-0 p-3 mt-10 w-full bg-gray-800 rounded-t-lg shadow sm:text-xs md:flex md:justify-between md:items-center md:p-5">
       <span className="text-sm text-gray-500 dark:text-gray-400 sm:text-center">
         © 2022{' '}
         <a
